3b9dbf8055bbfb86832bf60700c52acf2c1ddb0d
Poukam-Pierre <ngamaleu2011@gmail.com>
2024-03-08 20:56:06 +0100

    feat(app): create welcome component view

--- apps/donate/components/welcome/index.tsx
+++ apps/donate/components/welcome/index.tsx
@@ -1,7 +1,30 @@
-import { Typography } from "@mui/material";
+import { Box, Button, Typography } from "@mui/material";
 
 export default function Welcom_view() {
     return (
-        <Typography>I am a welcome view</Typography>
+        <Box>
+            <Typography variant="h2" sx={{
+                fontFamily: "Montserrat",
+                lineHeight: "36px",
+                fontSize: "30px",
+                letterSpacing: "-0.02em",
+                textAlign: "left",
+            }}> Giving Hope, Emplowerment, Love and Purpose.</Typography>
+            <Typography variant="h5" sx={{
+                fontFamily: "Montserrat",
+                lineHeight: "20px",
+                fontSize: "12px",
+                fontWeight: "600",
+                letterSpacing: "0em",
+                textAlign: "left"
+            }}> Nurturing Lives, Inspiring Futures: Transformative Initiative That Illuminate Paths, Foster Growth, Cultivate Affection, And Instill Purposeful Living For.</Typography>
+            <Box sx={{
+                display: 'flex',
+                justifyContent: "space-between"
+            }}>
+                <Button variant="contained">Faire un don</Button>
+                <Button variant="contained" color="secondary">Nous joindre</Button>
+            </Box>
+        </Box>
     )
 }
